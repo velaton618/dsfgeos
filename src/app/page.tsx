3610d5a24ce3c06bea114f9cfc44416fb20b718b
uaquax <uaquax@tutanota.com>
2024-05-22 15:16:20 +0800

    hamburger menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import s from "./page.module.sass";
-import { Search, ChevronDown, Hourglass, MountainIcon } from "lucide-react";
+import {
+  Search,
+  ChevronDown,
+  Hourglass,
+  MountainIcon,
+  Menu,
+  X,
+} from "lucide-react";
+import { useState } from "react";
 
 export default function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className={s.container}>
       <header className={s.header}>
@@ -42,7 +54,54 @@ export default function Home() {
           <input placeholder="Search..." />
           <Search className={s.icon} strokeWidth={1} />
         </div>
+        <button
+          className={s.menuBtn}
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+        >
+          {isMenuOpen ? <X size={32} /> : <Menu size={32} />}
+        </button>
       </header>
+      <div className={`${s.mobileHeader} ${isMenuOpen ? s.active : ""}`}>
+        <div className={s.background} />
+        <div className={s.content}>
+          <div className={s.logo}>
+            <Image alt="logo" src="/logo.png" width={50} height={44} />
+          </div>
+          <div className={s.search}>
+            <input placeholder="Search..." />
+            <Search className={s.icon} strokeWidth={1} />
+          </div>
+          <nav className={s.navigation}>
+            <div className={s.link}>
+              <a href="#">Technology</a>
+              <ChevronDown strokeWidth={1} />
+            </div>
+            <div className={s.link}>
+              <a href="#">Service</a>
+              <ChevronDown strokeWidth={1} />
+            </div>
+            <div className={s.link}>
+              <a href="#">Projects</a>
+              <ChevronDown strokeWidth={1} />
+            </div>
+            <div className={s.link}>
+              <a href="#">Scientific research</a>
+              <ChevronDown strokeWidth={1} />
+            </div>
+            <div className={s.link}>
+              <a href="#">Equipment</a>
+              <ChevronDown strokeWidth={1} />
+            </div>
+            <div className={s.link}>
+              <a href="#">Documents</a>
+              <ChevronDown strokeWidth={1} />
+            </div>
+            <div className={s.link}>
+              <a href="#">News</a>
+            </div>
+          </nav>
+        </div>
+      </div>
       <section className={s.main}>
         <div className={s.text}>
           <h1 className={s.title}>
@@ -146,11 +205,11 @@ export default function Home() {
       <footer className={s.footer}>
         <Image alt="logo" width={48} height={48} src={"/logo.png"} />
         <div className={s.text}>
-          <p className={s.info}>
+          <div className={s.info}>
             <p>Email: geopolariton@dsfgeos.cn</p>
             <br />
             <p>Adress: China, Hebei, Zhangjiakou, 河北帝斯杰奥科技有限公司</p>
-          </p>
+          </div>
           <p className={s.copyright}>
             © Copyright 2019-2024 2024, Hebei DSF-GEOS Technology Co., Ltd. All
             rights reserved.
