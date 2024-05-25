e7a437c684a91d67443ea177e8e96db3d65f00ec
uaquax <uaquax@tutanota.com>
2024-05-25 22:05:20 +0800

    Footer update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,19 +5,18 @@ import Image from "next/image";
 function Footer() {
   return (
     <footer className={s.footer}>
-      <Image alt="logo" width={48} height={48} src={"/logo.png"} />
+      <Image alt="logo" width={48} height={48} src={"/logo2.png"} />
       <div className={s.text}>
         <div className={s.info}>
           <p>Email: geopolariton@dsfgeos.cn</p>
-          <br />
-          <p>Adress: China, Hebei, Zhangjiakou, 河北帝斯杰奥科技有限公司</p>
+          {/* <br /> */}
+          <p>Adress: China, Hebei, Zhangjiakou</p>
         </div>
         <p className={s.copyright}>
           © Copyright 2019-{new Date().getFullYear()}, Hebei DSF-GEOS Technology
           Co., Ltd. All rights reserved.
         </p>
       </div>
-      <Image alt="logo" width={48} height={48} src={"/logo2.png"} />
     </footer>
   );
 }
