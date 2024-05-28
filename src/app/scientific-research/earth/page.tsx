f5f4f013774483e19f4dde30d33b177ff0c16772
uaquax <uaquax@tutanota.com>
2024-05-28 21:32:50 +0800

    Study of earth

--- src/app/scientific-research/earth/page.tsx
+++ src/app/scientific-research/earth/page.tsx
@@ -1,6 +1,26 @@
 import MarkdownPage from "@/components/MarkdownPage/MarkdownPage";
 
 const source = `
+# DSF-GEOS | Study of The Earth
+
+## Operational Forecast of Earthquake precursors
+<video width="100%" height="100%" controls>
+	<source src="/scientific-research/earth/video.mp4" type="video/mp4">
+</video>
+
+## GEOPHYSICAL INFORMATION ELECTRONIC RESOURCE
+![img](/scientific-research/earth/1.webp)
+![img](/scientific-research/earth/2.webp)
+![img](/scientific-research/earth/3.webp)
+
+## REGIONAL STUDIES OF THE SOUTH-EASTERN PART OF THE CASPIAN BASIN
+![img](/scientific-research/earth/4.webp)
+
+## REGIONAL STUDIES OF THE CENTRAL ASIAN FOLD BELT
+![img](/scientific-research/earth/5.webp)
+
+## STUDY OF THE DEEP STRUCTURE OF TURFAN AND DATONG VOLCANOES BY GPTS METHOD,CHINA
+![img](/scientific-research/earth/6.webp)
 `;
 
 function Page() {
