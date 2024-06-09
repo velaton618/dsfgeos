9c1fa7481b1ec4f4351ab84e071e628f8d39509a
uaquax <uaquax@tutanota.com>
2024-06-09 18:22:35 +0800

    Labarotory Testing

--- src/app/scientific-research/labaratory-testing/page.tsx
+++ src/app/scientific-research/labaratory-testing/page.tsx
@@ -1,6 +1,28 @@
 import MarkdownPage from "@/components/MarkdownPage/MarkdownPage";
 
 const source = `
+# DSF-GEOS Labaratory Testing
+
+<p style="text-align: center;">
+    LABORATORY TESTING WAS CONDUCTED WITH THE SUPPORT OF THE
+    <br> 
+    CHANGSHA URANIUM GEOLOGY RESEARCH INSTITUTE, CNNC
+</p>
+
+![image](/scientific-research/laboratory-testing/1.jpeg)
+<video width="100%" height="100%" controls>
+	<source src="/scientific-research/antarctic/video.mp4" type="video/mp4">
+</video>
+
+## METHODS AND EQUIPMENT
+![image](/scientific-research/laboratory-testing/2.webp)
+![image](/scientific-research/laboratory-testing/3.webp)
+
+## SPECTRUM OF ELECTROMAGNETIC RADIATION SAMPLES
+![image](/scientific-research/laboratory-testing/4.webp)
+
+## THE RESULTS OF LABORATORY STUDIES
+![image](/scientific-research/laboratory-testing/5.webp)
 `;
 
 function Page() {
