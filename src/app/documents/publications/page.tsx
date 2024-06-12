ac0895e6b02bb0073f0f1eb5fca15ab946a85923
uaquax <uaquax@tutanota.com>
2024-06-12 20:52:43 +0800

    publications

--- src/app/documents/publications/page.tsx
+++ src/app/documents/publications/page.tsx
@@ -1,12 +1,220 @@
 import MarkdownPage from "@/components/MarkdownPage/MarkdownPage";
 
 const source = `
+# DSF-GEOS: Publications
+
+- **Yu.A. Bogdanov, S.I. Prokopenko, O.O. Vodopianov, I.H. Azimov, and Xin Chen (2023).**  
+  Using the GPTS to explore the deep structure and prospects of oil and gas potential in the "Mezhdurechenskaya" area of the north-eastern side of the Ferhana Basin.  
+  *Conference:* 17th International Scientific Conference Monitoring of Geological Processes and Ecological Condition of the Environment  
+  *Location:* Kyiv, Ukraine  
+  [ISBN: 978-1-7138-9641-8](https://www.proceedings.com/74534.html)  
+  [DOI: 10.3997/2214-4609.2023520139](https://www.earthdoc.org/content/papers/10.3997/2214-4609.2023520139)
+
+- **I.K. Moldotashev, Yu.A. Bogdanov, A. A. Sorokin (2023).**  
+  Evaluation of the possibility of using spectral analysis of a single-channel ECG for the diagnosis of diastolic dysfunction of the left ventricle of the heart.  
+  *Journal:* Heart Vessels and Transplantation 2023; 7  
+  [ISSN: 1694-7886 (PRINT)](https://www.hvt-journal.com/)  
+  [DOI: 10.24969/hvt.2023.424](https://www.hvt-journal.com/articles/art424)
+
+- **S.I. Prokopenko, O.O. Vodopianov, and Xin Chen (2022).**  
+  Using the geopolariton tomography based on UAV to explore and monitor coalfield subsurface structures in Shaanxi, China.  
+  *Conference:* 16th International Scientific Conference Monitoring of Geological Processes and Ecological Condition of the Environment  
+  *Location:* Kyiv, Ukraine  
+  [ISBN: 978-1-7138-7117-0](https://www.proceedings.com/68440.html)  
+  [DOI: 10.3997/2214-4609.2022580080](https://www.earthdoc.org/content/papers/10.3997/2214-4609.2022580080)
+
+- **Xin Chen, S.I. Prokopenko, Yu.A. Bogdanov, and O.O. Vodopianov (2021).**  
+  Application of the GeoPolariton Tomography based on UAV in prediction and monitoring of mountainous coal spontaneous combustion area in eastern Yunnan Plateau.  
+  *Conference:* 1st China Exploration GeoScience Conference (CEGC)  
+  *Location:* Hefei, China  
+  [DOI: 10.26914/c.cnkihy.2021.036284](https://kns.cnki.net/kcms2/article/abstract?v=LD-wYsOa3DgSNcriii-WXKU2F5x9pP-0QNCTljC6DYz053CLorzMkYPLbMDTzwmzDPgHL73JMbuXMrI7oHG-bxKHxqzvZLdY7DU6go91u7V3ui42-oK6H9nLifVVvOh2Zgm_y4ZG4e7uwnaudgxDWw==&uniplatform=NZKPT&language=CHS)  
+  [ResearchGate Link](https://www.researchgate.net/publication/355204254_Application_of_the_GeoPolariton_Tomography_based_on_UAV_in_prediction_and_monitoring_of_mountainous_coal_spontaneous_combustion_area_in_eastern_Yunnan_Plateau)
+
+- **S.I. Prokopenko, Yu.A. Bogdanov, O.O. Vodopianov, and Xin Chen (2021).**  
+  GEOPOLARITON TOMOGRAPHY (GPTS) HARDWARE AND SOFTWARE PLATFORM FOR STUDY OF EARTH'S DEEP STRUCTURE.  
+  *Conference:* XX th International Conference Geoinformatics: Theoretical and Applied Aspects  
+  *Location:* Kiev, Ukraine  
+  [ISBN: 978-1-7138-3368-0](https://www.proceedings.com/59927.html)  
+  [DOI: 10.3997/2214-4609.20215521012](https://www.earthdoc.org/content/papers/10.3997/2214-4609.20215521012)
+
+- **陈鑫, Prokopenko Serhiy, and Bogdanov Yuriy (2020).**  
+  大地极化激元层析成像技术(GPTS )的研究.  
+  *Journal:* 河北省高新技术企业国际科技合作技术创新成果案例 2020, P. 171-176  
+  [ISBN: 978-7-5717-0538-1](https://m.cxstar.com/book/28c9dd6c0000e1XXXX)  
+  [ResearchGate Link](https://www.researchgate.net/publication/349774031_dadejihuajiyuancengxichengxiangjishuGPTS_deyanjiu)
+
+- **Yu.A. Bogdanov, S.I. Prokopenko, and Xin Chen (2020).**  
+  Use of GPTS technology in geoinformation security for sustainable development of megacities.  
+  *Conference:* XIX th International Conference Geoinformatics: Theoretical and Applied Aspects  
+  *Location:* Kiev, Ukraine  
+  [ISBN: 978-1-7138-1398-9](https://www.proceedings.com/55189.html)  
+  [DOI: 10.3997/2214-4609.2020geo134](https://www.earthdoc.org/content/papers/10.3997/2214-4609.2020geo134)
+
+- **E.A. Trubacheva, Yu.A. Bogdanov (2022).**  
+  IDENTEFICATION OF SIGNS OF COVID-19 INFECTION IN THE ECG BY CHANGES IN THE FREQUENCY SPECTRUM IN THE BASIS OF THE LAGERRA POLYNOMIALS.  
+  *Journal:* Вестник неотложной и восстановительной медицины.- 2022.-Т2, №1.- С. 61-78.  
+  [ISSN: 2306-4110](https://www.researchgate.net/publication/348651210_VYAVLENIE_PRIZNAKOV_INFICIROVANIA_COVID-19_V_EKG_PO_IZMENENIAM_CASTOTNOGO_SPEKTRA_V_BAZISE_MNOGOCLENOV_LAGERRA_Vestnik_neotloznoj_i_vosstanovitelnoj_mediciny-_2020-T2_No1-_S_61-78_ISSN_2306-4110)
+
+- **Yu.A. Bogdanov, S.I. Prokopenko, I.N. Fedotova, and Xin Chen (2019).**  
+  The study of the deep structure and prospects of oil and gas potential of the southern part of the Guryev arch of the south-eastern side of the Caspian basin by method GPTS.  
+  *Conference:* Third international conference on Geology of the Caspian Sea and Adjacent Areas  
+  *Location:* Oct 2019  
+  *Volume:* 2019, p.1-6  
+  [ISBN: 978-1-7138-1179-4](https://www.proceedings.com/content/054/054525webtoc.pdf)  
+  [DOI: 10.3997/2214-4609.201952023](https://www.earthdoc.org/content/papers/10.3997/2214-4609.201952023)
+
+- **李进宝, 梁玉森, 柴利娜 (2022).**  
+  大地极化子勘探技术在煤矿领域的应用初探, 四川地质学报  
+  *Journal:* ISSN：1006-0995 年,卷(期): 2022,42(1), 页数: 4 (150-153)  
+  [DOI: 10.3969/j.issn.1006-0995.2022.01.029](https://d.wanfangdata.com.cn/periodical/ChlQZXJpb2RpY2FsQ0hJTmV3UzIwMjMwNDI2Eg9zY2R6eGIyMDIyMDEwMjkaCGFtbmt4M3hw)
+
+- **胥值礼, 孟庆敏, 崔志强, 高卫东 (2014).**  
+  大地极化声子测深技术的调研与试用  
+  *Journal:* 中国地质科学院地球物理地球化学勘查研究所, 河北廊坊 065000, Vol. 38 Issue (4): 758-763  
+  *Institute:* Institute of Geophysical and Geochemical Exploration, CAGS, Langfang 065000, China  
+  [DOI: 10.11720/wtyht.2014.4.23](http://www.wutanyuhuatan.com/CN/10.11720/wtyht.2014.4.23)
+
+- **В.Н. Шуман, В.П. Коболев, Ю.О. Богданов, І.Г. Захаров, Д.А. Яцюта (2011).**  
+  Спонтанне електромагнітне випромінювання на акваторіях: новий експеримент та використання.  
+  *Conference:* 10th EAGE International Conference on Geoinformatics - Theoretical and Applied Aspects  
+  *Location:* May 2011  
+  [DOI: 10.3997/2214-4609.20145076](https://www.earthdoc.org/content/papers/10.3997/2214-4609.20145076)
+
+- **Yu.A. Bogdanov, V.N. Shuman, V.N. Pavlovych, V. Kobo, and A.Z. Ganiev (2010).**  
+  Monitoring of the seism-electromagnetic Earth background in connection with seismic activity.  
+  *Conference:* 9th EAGE International Conference on Geoinformatics - Theoretical and Applied Aspects  
+  *Location:* May 2010  
+  [ISBN: 978-1-63266-991-9](https://www.proceedings.com/23103.html)  
+  [DOI: 10.3997/2214-4609.201402823](https://www.earthdoc.org/content/papers/10.3997/2214-4609.201402823)
+
+- **Yu.A. Bogdanov, I.B. Burkinsky, V.P. Kobolev, and O.M. Rusakov (2008).**  
+  Multi-component Analysis of Radio Wave Background of the Earth Reveals Hydrocarbon Accumulations in the Black Sea Basin.  
+  *Conference:* EAGE International Conference on Petroleum Geology and Hydrocarbon Potential of Caspian and Black Sea Regions  
+  *Location:* Oct 2008  
+  [ISBN: 978-1-63266-994-0](https://www.proceedings.com/content/023/023106webtoc.pdf)  
+  [DOI: 10.3997/2214-4609.20146112](https://www.earthdoc.org/content/papers/10.3997/2214-4609.20146112)
+
+- **Yu.A. Bogdanov, I.G. Zakharov, O.F. Turnov, and M. Hauakawa (2003).**  
+  ELEKTROMAGNETIC EFFECTS ASSOCIATED WITH REGIONAL SEISMIC ACTIVITY IN CRIMEA DURING THE INTERVAL JULY-AUGUST 2002.  
+  *Journal:* Journal of Atmospheric Electricity, JAPAN, Vol.23 №2  
+  [DOI: 10.1615/TelecomRadEng.v58.i1-2.150](http://www.dl.begellhouse.com/journals/0632a9d54950b268,112e3cab6d459484,4fd828d75bd0e7ef.html)
+
+- **Ю.О. Богданов та С.І. Прокопенко (2018).**  
+  ВИВЧЕННЯ ТРУБОПОДІБНИХ СТРУКТУР ШЕЛЬФУ ПІВДЕННО-КИТАЙСЬКОГО МОРЯ МЕТОДОМ ГЕОПОЛЯРИТОННОЇ ТОМОГРАФІЇ ЗЕМЛІ.  
+  *Conference:* Міжнародна наукова конференція морські геолого-геофізичні дослідження фундаментальні та прикладні аспекти, Одеса, 295-303.  
+  [ResearchGate Link](https://www.researchgate.net/publication/334883867_IZUCENIE_TRUBOOBRAZNYH_STRUKTUR_SELFA_UZNO-KITAJSKOGO_MORA_METODOM_GEOPOLARITONNOJ_TOMOGRAFII_ZEMLI)
+
+- **Т.К. Бурахович, В.П. Коболев (2017).**  
+  Актуальные проблемы геосреды и зондирующих систем.  
+  *Journal:* Геофизический журнал, Т. 39, № 6, С. 139-143.  
+  [DOI: 10.24028/gzh.0203-3100.v39i6.2017.116946](http://dspace.nbuv.gov.ua/handle/123456789/127676)
+
+- **Ю.А. Богданов (2017).**  
+  ПОСТРОЕНИЕ МОДЕЛИ СКОРОСТИ РАСПРОСТРАНЕНИЯ УПРУГОЙ ВОЛНЫ НА ОСНОВАНИИ ТРАСС ДАННЫХ ГЕОПОЛЯРИТОННОГО ЗОНДИРОВАНИЯ.  
+  [ResearchGate Link](https://www.researchgate.net/publication/335973205_UDK_55034_POSTROENIE_MODELI_SKOROSTI_RASPROSTRANENIA_UPRUGOJ_VOLNY_NA_OSNOVANII_TRASS_DANNYH_GEOPOLARITONNOGO_ZONDIROVANIA)
+
+- **Ю.А. Богданов и А.А. Водопьянов (2017).**  
+  ИСПОЛЬЗОВАНИЕ ЛАГГЕРР-СПЕКТРАЛЬНОГО АНАЛИЗА В ПАССИВНОЙ ГЕОПОЛЯРИТОННОЙ ТОМОГРАФИИ.  
+  [ResearchGate Link](https://www.researchgate.net/publication/335973414_LAGUERRE-SPECTRAL_ANALYSIS_USING_PASSIVE_GEOPOLARITON_TOMOGRAPHY)
+
+- **С.И. Прокопенко (2017).**  
+  ПРИМЕНЕНИЕ ПАССИВНОГО ГЕОФИЗИЧЕСКОГО СКАНЕРА «DSF» ДЛЯ ИССЛЕДОВАНИЯ ГЛУБИННОГО СТРОЕНИЯ ЗЕМЛИ.  
+  *Conference:* Національна академія наук України, Інститут геофізики ім. С.І. Субботина, Матеріали третьої міжнародної наукової конференції "Актуальні проблеми геосереди і зондувальних систем", 3-5 жовтня, Київ, Україна  
+  [ResearchGate Link](https://www.researchgate.net/publication/334883471_PRIMENENIE_PASSIVNOGO_GEOFIZICESKOGO_SKANERA_DSF_DLA_ISSLEDOVANIA_GLUBINNOGO_STROENIA_ZEMLI)
+
+- **Ю.А. Богданов и С.И. Прокопенко (2015).**  
+  КОЛЬЦЕВАЯ СТРУКТУРА ЦАЙДАМСКОЙ КОТЛОВИНЫ (КИТАЙ) ПО РЕЗУЛЬТАТАМ ПАССИВНОГО ЭЛЕКТРОМАГНИТНОГО ПРОФИЛИРОВАНИЯ.  
+  *Conference:* Современные проблемы геологии, Материалы научно-практической конференции к 100 летию до дня рождения В.П. Макридина, Харьков  
+  [ResearchGate Link](https://www.researchgate.net/publication/335988435_KOLCEVAA_STRUKTURA_CAJDAMSKOJ_KOTLOVINY_KITAJ_PO_REZULTATAM_PASSIVNOGO_ELEKTROMAGNITNOGO_PROFILIROVANIA)
+
+- **Ю.А. Богданов, А.М. Черняков, И.Н. Федотова, С.И. Прокопенко, И.Н. Самчук (2015).**  
+  НЕРЕАЛИЗОВАННЫЕ ПЕРСПЕКТИВЫ ПОИСКА УГЛЕВОДОРОДОВ В ЮГО-ВОСТОЧНОЙ ЧАСТИ ДНЕПРОВСКО-ДОНЕЦКОЙ ВПАДИНЫ.  
+  *Conference:* Современные проблемы геологии, Материалы научно-практической конференции к 100 летию до дня рождения В.П. Макридина, Харьков  
+  [ResearchGate Link](https://www.researchgate.net/publication/335988208_NEREALIZOVANNYE_PERSPEKTIVY_POISKA_UGLEVODORODOV_V_UGO-_VOSTOCNOJ_CASTI_DNEPROVSKO-DONECKOJ_VPADINY)
+
+- **Yu.A. Bogdanov, V. Pavlovych, S.I. Prokopenko, D. Gergova, B. Vachev (2013).**  
+  ELECTROMAGNETIC SOUNDING OF THRACIAN TUMULI IN THE SBORYANOVO NATIONAL RESERVE, NEAR THE TOWN OF ISPERIH, RAZGRAD DISTRICT, NORTH EAST BULGARIA.  
+  [ResearchGate Link](https://www.researchgate.net/publication/335856518_ELECTROMAGNETIC_SOUNDING_OF_THRACIAN_TUMULI_IN_THE_SBORYANOVO_NATIONAL_RESERVE_NEAR_THE_TOWN_OF_ISPERIH_RAZGRAD_DISTRICT_NORTH_EAST_BULGARIA_BlackSeaHazNet_Scientific_Report_-_EU_FP7_IRSES_Project_201)
+
+- **Yu.A. Bogdanov, V.O. Pavlovych, I. Kvasniuk (2013).**  
+  POSSIBILITY OF EARTH STATE DIAGNOSTICS AND CRUST STRUCTURE STUDY BY ANALYZING ELECTROMAGNETIC RADIATION OF EARTH.  
+  [ResearchGate Link](https://www.researchgate.net/publication/335961352_POSSIBILITY_OF_EARTH_STATE_DIAGNOSTICS_AND_CRUST_STRUCTURE_STUDY_BY_ANALYZING_ELECTROMAGNETIC_RADIATION_OF_EARTH_SHORT_TITLE_POSSIBILITY_OF_EARTH_STATE_DIAGNOSTICS)
+
+- **Yu.A. Bogdanov (2011).**  
+  Experimental studies of the Earth's own oscillations in Antarctica and Ukraine in electromagnetic radiation.  
+  *Journal:* Geofizicheskii Zhurnal (Geophysical Journal) 11(80):90  
+  [DOI: 10.24028/gzh.0203-3100.v33i1.2011.117328](https://www.researchgate.net/publication/335972697_Experimental_studies_of_the_Earth's_own_oscillations_in_Antarctica_and_Ukraine_in_electromagnetic_radiation)
+
+- **V.M. Pavlovych, Yu.A. Bogdanov, V.M. Shuman, V.M. Vaschenko (2009).**  
+  ELECTRODYNAMICS OF TECTONICS PROCESSES AND ELECTROMAGNETIC PROFILING OF THE EARTH CRUST IN ANTARCTIC REGION.  
+  *Journal:* Geofizicheskii Zhurnal 2009-№8  
+  [ResearchGate Link](https://www.researchgate.net/publication/335961105_ELECTRODYNAMICS_OF_TECTONICS_PROCESSES_AND_ELECTROMAGNETIC_PROFILING_OF_THE_EARTH_CRUST_IN_ANTARCTIC_REGION_1_2_3_4_2)
+
+- **Ю.А. Богданов и А.М. Черняков (2009).**  
+  ВЕРОЯТНАЯ ПРИЧИНА ГАЗОВЫХ ВЫБРОСОВ В ШАХТАХ ДОНБАССА И ВОЗМОЖНЫЕ ПУТИ ИХ ИЗУЧЕНИЯ.  
+  *Journal:* Доповіді Національної академії наук України  
+  [ResearchGate Link](https://www.researchgate.net/publication/335972712_Veroatnaa_pricina_gazovyh_vybrosov_v_sahtah)
+
+- **Ю.А. Богданов, В.Н. Павлович, В.Н. Шуман (2009).**  
+  СПОНТАННАЯ ЭЛЕКТРОМАГНИТНАЯ ЭМИССИЯ ЛИТОСФЕРЫ.  
+  *Journal:* Геофизический журнал 2009, Том 31 №4, Институт геофизики НАН Украины им. С.И.Субботина, Киев, Украина  
+  [ResearchGate Link](https://www.researchgate.net/publication/335992598_Spontannaa_elektromagnitnaa_emissia_litosfery_sostoanie_problemy_i_matematiceskie_modeli)
+
+- **Г.В. Алешин и Ю.А. Богданов (2009).**  
+  ЭФФЕКТИВНОСТЬ СЛОЖНЫХ РАДИОТЕХНИЧЕСКИХ СИСТЕМ.- Киев.  
+  [ResearchGate Link](https://www.researchgate.net/publication/335961248_Effektivnost_sloznyh_radio-tehniceskih_sistem)
+
+- **Ю.А. Богданов, В.Н. Павлович (2009).**  
+  НЕРАВНОВЕСНОЕ ИЗЛУЧЕНЕ ЗЕМНОЙ КОРЫ—ИНДИКАТОР ГЕОДИНАМИЧЕСКИХ ПРОЦЕССОВ.  
+  *Journal:* Геофиз.журн Т30, № 4., 12—24  
+  [ResearchGate Link](https://www.researchgate.net/publication/335972571_Neravnovesnoe_izlucenie_zemnoj_kory_-_indikator_geodinamiceskih_processov)
+
+- **В.П. Коболев, Ю.А. Богданов (2007).**  
+  КОЛЬЦЕВАЯ СТРУКТУРА В ЧЁРНОМ МОРЕ К ЗАПАДУ ОТ ТАРХАНКУТСКОГО П-ОВА: АСТРОБЛЕМА.  
+  *Conference:* Российская академия наук, Институт океанологии им. П.П. Ширшова, Геология морей и океанов Москва, Том 4. Материалы 17 Международной конференции по морской геологии, Москва, Стр. 105-107  
+  [ResearchGate Link](https://www.researchgate.net/publication/336058229_Kolcevaa_struktura_v_Cernom_more_k_zapadu_ot_Tarhankutskogo_p-ova_astroblema)
+
+- **Ю.А. Богданов, В.П. Коболев, О.М. Русаков, И.Г. Захаров (2007).**  
+  ГЕОПОЛЯРИТОННОЕ ЗОНДИРОВАНИЕ СТРУКТУР СЕВЕРО-ЗАПАДНОГО ШЕЛЬФА ЧЕРНОГО МОРЯ.  
+  *Journal:* Геология и полезные ископаемые мирового океана №1  
+  [ResearchGate Link](https://www.researchgate.net/publication/342864244_GEOPOLARITONNOE_ZONDIROVANIE_STRUKTUR_SEVERO-ZAPADNOGO_SELFA_CERNOGO_MORA)
+
+- **Yu.A. Bogdanov, V.M. Vaschenko, E.A. Loza, Jh.I. Patlashenko (2006).**  
+  NEW AEROCOSMIC TECHNOLOGIES OF DISTANCE EARTH SURFACE AND INTERIOR SOUNDING.  
+  [ResearchGate Link](https://www.researchgate.net/publication/329028777_New_aerocosmic_technologies_of_Earth_surface_and_interior_remote_sounding)
+
+- **Г.В. Алешин, Ю.А. Богданов, А.В. Коломийцев (2002).**  
+  ОЦЕНКА ТОЧНОСТИ ИЗМЕРИТЕЛЕЙ ЧАСТОТЫ В УСЛОВИЯХ ВОЗДЕЙСТВИЯ ФЛУКТУАЦИОННЫХ ПОМЕХ.  
+  *Journal:* ISSN 1681-7710. Системи обробки інформації, выпуск 2 (18)  
+  [ResearchGate Link](https://www.researchgate.net/publication/335993341_OCENKA_TOCNOSTI_IZMERITELEJ_CASTOTY_V_USLOVIAH_VOZDEJSTVIA_FLUKTUACIONNYH_POMEH)
+
+- **Янушкевич, В.Ф. (2017).**  
+  Электромагнитные методы поиска и идентификации углеводородных залежей.  
+  *Publisher:* Новополоцк : Полоц. гос. ун-т, 2017, 232 с.  
+  [Download File](https://elib.psu.by/handle/123456789/22036)
+
+- **V.N. Shuman (2017).**  
+  SELECTED WORKS.  
+  *Publisher:* National Academy of Sciences of Ukraine, Institute of Geophysics, Kiev  
+  [Download File](https://dsfgeos.cn/files/Shuman-2017.pdf)
+
+- **А.М. Черняков (2015).**  
+  ГЕОЛОГИЯ И ЖИЗНЬ ФОРМУЛА УСПЕХА.  
+  *Publisher:* Харьков, ХНМУ, 120  
+  [Download File](https://dsfgeos.cn/files/Chernyakov-2015.pdf)
+
+- **BlackSeaHazNet Conclusion Workshop (2013).**  
+  16-20 December, 2013, Sofia, Bulgaria  
+  *Project:* BlackSeaHazNet FP7 IRSES Project 246874 ”Complex Research of Earthquake’s Forecasting Possibilities, Seismicity and Climate Change Correlations”  
+  [Download File](https://dsfgeos.cn/files/book_2013.pdf)
+
+- **J. Bialas, S. Mykhaylyuk, A. Vasiliev, I. Klaucke, C. Papenberg, K. Schwalenberg (2011).**  
+  The experimental-methodical geophysical studies in the RV POSEIDON cruise 405  
+  [Download File](https://www.caass.rs/objavljen-zbornik-radova-sa-konferencije-cche-2024/)
 `;
 
 function Page() {
-  return (
-    <MarkdownPage source={source}/>
-  );
+  return <MarkdownPage source={source} />;
 }
 
 export default Page;
