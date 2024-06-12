78cd445bdbe1286ced7c8e42f0ffcda16335cb73
uaquax <uaquax@tutanota.com>
2024-06-12 20:45:40 +0800

    certificates

--- src/app/documents/certificates/page.tsx
+++ src/app/documents/certificates/page.tsx
@@ -1,12 +1,21 @@
 import MarkdownPage from "@/components/MarkdownPage/MarkdownPage";
 
 const source = `
+# DSF-GEOS: Certificates
+
+![img](/documents/certificates/1.jpeg)
+![img](/documents/certificates/2.jpeg)
+![img](/documents/certificates/3.jpeg)
+![img](/documents/certificates/4.webp)
+![img](/documents/certificates/5.webp)
+![img](/documents/certificates/6.webp)
+![img](/documents/certificates/7.webp)
+![img](/documents/certificates/8.webp)
+
 `;
 
 function Page() {
-    return (
-        <MarkdownPage source={source} />
-    );
+  return <MarkdownPage source={source} />;
 }
 
 export default Page;
