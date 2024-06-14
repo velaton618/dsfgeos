de110ab7a4af0c531f791bcdb006f8c5166cb822
uaquax <uaquax@tutanota.com>
2024-06-14 12:45:38 +0800

    fix about

--- src/app/equipment/dsf-scanner/page.tsx
+++ src/app/equipment/dsf-scanner/page.tsx
@@ -1,22 +1,7 @@
 import MarkdownPage from "@/components/MarkdownPage/MarkdownPage";
 
 const source = `
-# DSF-GEOS: SCANNER DSF
-
-## DEEP SUBSURFACE STRUCTURE SURVEY SCANNER "DSF"
-![image](/equipment/scanner/1.webp)
-
-## TECHNICAL SPECIFICATIONS
-
-| Parameter | Designation |
-|-----------|-------------|
-| **1 Hardware parameters** | |
-| 1.1 Frequency range | from 1 to 100kHz |
-| 1.2 The sensitivity given to the antenna input of the complex | is not more than 1*10<sup>-6</sup> A/m |
-| 1.3 The time of continuous recording into the internal memory of the complex at a measurement frequency of 10 Hz | 12h |
-| 1.4 Continuous operation time from a fully charged battery | 20h |
-| **2 Degree of protection of enclosures:** | IP65 |
-| **3 Temperature range:** | |
+# DSF-GEOS: SCANNER DSF ## DEEP SUBSURFACE STRUCTURE SURVEY SCANNER "DSF" ![image](/equipment/scanner/1.webp) ## TECHNICAL SPECIFICATIONS | Parameter | Designation | |-----------|-------------| | **1 Hardware parameters** | | | 1.1 Frequency range | from 1 to 100kHz | | 1.2 The sensitivity given to the antenna input of the complex | is not more than 1*10<sup>-6</sup> A/m | | 1.3 The time of continuous recording into the internal memory of the complex at a measurement frequency of 10 Hz | 12h | | 1.4 Continuous operation time from a fully charged battery | 20h | | **2 Degree of protection of enclosures:** | IP65 | | **3 Temperature range:** | |
 | - operating and short-term temperature | от -30°С до +60°С |
 | - long-term storage temperature | от -40°С до +70°С |
 | **4 Weight, kg, no more:** | |
@@ -38,9 +23,7 @@ const source = `
 `;
 
 function Page() {
-  return (
-    <MarkdownPage source={source}/>
-  );
+  return <MarkdownPage source={source} />;
 }
 
 export default Page;
